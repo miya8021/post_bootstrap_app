93e74ba70ac4828263c829d62ab50f4b6be44718
miya <rinn.sasami@gmail.com>
2021-02-02 18:22:46 +0900

    削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,11 +23,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to root_path
   end
 
   private
 
   def post_params
-    params.require(:post).premit(:title, :content)
+    params.require(:post).permit(:title, :content)
   end
 end
